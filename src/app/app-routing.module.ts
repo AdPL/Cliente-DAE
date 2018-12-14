a52258c9cb748a459d702004520ee235c7ab0429
Adrian Perez <adrianpelopez@gmail.com>
2018-12-14 17:16:23 +0100

    Agregada gestión de rutas (routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventoConsultaComponent } from './evento-consulta/evento-consulta.component';
+import { EventosListaComponent } from './eventos-lista/eventos-lista.component';
+import { UsuarioConsultaComponent } from './usuario-consulta/usuario-consulta.component';
+import { UsuarioRegistrarComponent } from './usuario-registrar/usuario-registrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'consulta', component: EventoConsultaComponent },
+  { path: 'eventos', component: EventosListaComponent },
+  { path: 'usuarios/:nombre', component: UsuarioConsultaComponent },
+  { path: 'registro', component: UsuarioRegistrarComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
