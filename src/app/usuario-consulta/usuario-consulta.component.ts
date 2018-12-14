6743c3cd74dd4151a550805b42507279c971f216
Adrian Perez <adrianpelopez@gmail.com>
2018-12-14 17:17:59 +0100

    Actualizada consulta de usuario para obtener datos de ActivatedRoute

--- src/app/usuario-consulta/usuario-consulta.component.ts
+++ src/app/usuario-consulta/usuario-consulta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from '../usuario';
 import { UsuariosService } from '../usuarios.service';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-usuario-consulta',
@@ -12,11 +13,22 @@ export class UsuarioConsultaComponent implements OnInit {
   usuario: Usuario;
   error: boolean;
 
+  param: string = "";
+
   form: FormGroup;
   nombre = new FormControl('');
   payload = '';
 
-  constructor(private usuarioService: UsuariosService) { }
+  constructor( private usuarioService: UsuariosService, private route: ActivatedRoute ) { }
+
+  getDatosUsuario(nombre: string): void {
+    this.usuarioService.getUsuario(nombre)
+        .subscribe(
+          usuario => this.usuario = usuario,
+          error => this.error = true,
+          () => this.error = false
+    );
+  }
 
   getUsuario(): void {
     this.usuarioService.getUsuario(this.nombre.value)
@@ -28,6 +40,10 @@ export class UsuarioConsultaComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.param = this.route.snapshot.params.nombre;
+    this.usuarioService.getUsuario(this.param)
+        .subscribe(
+          usuario => this.usuario = usuario
+    );
   }
-
 }
